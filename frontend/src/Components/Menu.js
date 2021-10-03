5a4bf79ef20fcadb65237a069901ce4c0850bc4d
Manan Sahni <manansahni567@gmail.com>
2021-10-03 21:39:46 +0530

    Bug Fixes

--- frontend/src/Components/Menu.js
+++ frontend/src/Components/Menu.js
@@ -7,9 +7,17 @@ function Menu(props) {
 	var [list,setList]=useState([0]);
 	axios.get("/loadmenu").then((res) => {
 		var l=res.data;
+		if(JSON.parse(localStorage.getItem("order")).length!==l.length)
+		{
+			var li=[]
+			list.forEach((item) => {li.push(0)})
+			localStorage.setItem("order",JSON.stringify(li));
+			localStorage.setItem("total",0);
+		}
 		localStorage.setItem("list",JSON.stringify(l));
 		setList(l);
 	})
+	
 	return <div className="Menu">
 		<p className="menu"> Menu </p>
 		{list.map((item,key) => <Item item={item} index={key} />)}
